97871bf8bcebbc15dbfb146a5ac9f2144ccdd3b4
Janepop Atirattanachai <jane3030@gmail.com>
2024-05-19 22:54:32 +0700

    optimize import

--- src/main/java/com/devpuccino/accountservice/domain/request/CategoryRequest.java
+++ src/main/java/com/devpuccino/accountservice/domain/request/CategoryRequest.java
@@ -1,7 +1,5 @@
 package com.devpuccino.accountservice.domain.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
