2cd8ffe9ff1fc6fe15d612e3f1fbbfe2bc3a5dff
Janepop Atirattanachai <jane3030@gmail.com>
2024-06-01 23:27:05 +0700

    add throwable

--- src/main/java/com/devpuccino/accountservice/repository/CategoryRepository.java
+++ src/main/java/com/devpuccino/accountservice/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.devpuccino.accountservice.repository;
 
 import com.devpuccino.accountservice.entity.CategoryEntity;
+import com.devpuccino.accountservice.exception.DuplicateDataException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Integer> {
-    List<CategoryEntity> findByCategoryName(String categoryName);
+    List<CategoryEntity> findByCategoryName(String categoryName) throws Exception;
 }
