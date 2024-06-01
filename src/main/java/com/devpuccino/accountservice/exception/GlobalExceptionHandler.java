33aea21e2c0a6afccc9a4cb66eb98eab13362254
Janepop Atirattanachai <jane3030@gmail.com>
2024-06-01 23:26:17 +0700

    write log error

--- src/main/java/com/devpuccino/accountservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devpuccino/accountservice/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.devpuccino.accountservice.exception;
 
 import com.devpuccino.accountservice.domain.response.CommonResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Component
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    private static Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(Exception.class)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public CommonResponse handleException(Exception exception){
+        logger.error(exception.getMessage(),exception);
         CommonResponse response = new CommonResponse();
         response.setCode("500-001");
         response.setMessage("Internal server error");
