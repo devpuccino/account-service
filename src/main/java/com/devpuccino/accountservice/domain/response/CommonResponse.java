48f233614c1df773019e428cb62cf8290de4e030
Janepop Atirattanachai <jane3030@gmail.com>
2024-06-02 22:48:08 +0700

    add data field

--- src/main/java/com/devpuccino/accountservice/domain/response/CommonResponse.java
+++ src/main/java/com/devpuccino/accountservice/domain/response/CommonResponse.java
@@ -2,8 +2,11 @@ package com.devpuccino.accountservice.domain.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class CommonResponse {
     private String code;
     private String message;
+    private List<Category> data;
 }
